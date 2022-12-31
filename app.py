1c3a056770035a64d2e48926328af48d16799b97
Pramod kumar Nagaraj <82264044+Pramod-Kumar808@users.noreply.github.com>
2022-12-31 20:24:14 +0100

    sessions

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import streamlit as st
 import requests
 from bs4 import BeautifulSoup
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
 st.title("Run test")
 
 URL = "http://10.1.0.3/"
-page = requests.get(URL, verify=False, timeout=1)
+
+session = requests.Session()
+retry = Retry(connect=3, backoff_factor=0.5)
+adapter = HTTPAdapter(max_retries=retry)
+session.mount('http://', adapter)
+session.mount('https://', adapter)
+page = session.get(URL, verify=False, timeout=1)
 st.code(page.text)
